1bab36b0ae0b891e8102c2d9d3569adbeb6fea94
Cristian <CristianGualteros23@gmail.com>
2020-04-12 23:45:15 -0500

    docs

---
+++ docs/index.php
@@ -0,0 +1,20 @@
+<?php
+
+ echo 'Hola mundo';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-group">
+        <label for="my-select">Text</label>
+        <select id="my-select" class="custom-select" name="">
+            <option>Text</option>
+        </select>
+    </div>
+</body>
+</html>
